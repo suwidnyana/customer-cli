d1cdfbff1ec8f45d4d981599d389eaf68e4a86f4
suwidnyana <grayfullbuster522@gmail.com>
2020-04-24 03:54:35 +0800

    command mungkin beda huruf besar dan kecil

---
+++ command.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+const program = require('commander')
+const { prompt } = require('inquirer')
+const {
+    addCustomer,
+    findCustomer,
+    updateCustomer,
+    removeCustomer,
+    listCustomer
+
+} = require('./index')
+
+
+const pertanyaan = [
+
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'Nama Pertama'
+    },
+     
+    {
+        type: 'input',
+        name: 'lastName',
+        message: 'Nama Terakhir'
+    },
+
+    {
+        type: 'input',
+        name: 'phone',
+        message: 'No Telepon'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email'
+    }
+
+]
+
+// program
+//     .command('add <firstname> <lastname> <phone> <email>')
+//     .alias('a')
+//     .description('Tambah Pelanggan')
+//     .action((firstName, lastName, phone, email) => {
+//         addCustomer({
+//             firstName,
+//             lastName,
+//             phone,
+//             email
+//         });
+//     })
+
+    program
+        .command('add')
+        .alias('a')
+        .description('Tambahkan Customer')
+        .action(() => {
+            prompt(pertanyaan)
+                .then(jawaban => addCustomer(jawaban))
+        })
+
+//Update Customer
+
+        program
+        .command('update <_id>')
+        .alias('u')
+        .description('Update a Customer')
+        .action((_id) => {
+            prompt(pertanyaan)
+                .then(jawaban => updateCustomer(_id,jawaban))
+        })
+
+//Remove Customer
+        program
+        .command('remove <_id>')
+        .alias('r')
+        .description('Remove A customer')
+        .action(_id => removeCustomer(_id) )
+
+//List Customer
+        program
+        .command('List')
+        .alias('l')
+        .description('List Customers')
+        .action(() => listCustomer())
+
+
+    program
+    .command('find <name>')
+    .alias('f')
+    .description('Find a customer')
+    .action(name => findCustomer(name));
+
+
+program 
+    .version('1.0.0')
+    .description('Client Management System')
+
+program.parse(process.argv)
---
+++ index.js
@@ -0,0 +1,98 @@
+ const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise
+
+ const db = mongoose.connect('mongodb://localhost:27017/customercli', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+ })
+
+//Import Model
+ const Customer = require('./models/customer');
+
+
+
+ //Add Customer
+//  const addCustomer = (customer,) => {
+//     Customer.create(customer).then(customer => {
+//       console.info('New Customer Added');
+//       db.close();
+//     });
+//   }
+
+  const addCustomer = async (customer) => {
+   try {
+       Customer.create(customer)
+        .then(customer => {
+         console.info('New Customer Added');
+         mongoose.connection.close()
+       });
+
+   } catch ( err ) {
+       console.info(err)
+   }
+  }
+
+
+//Find Customer
+const findCustomer = (name) => {
+    // Make case insensitive
+    const search = new RegExp(name, 'i');
+    Customer.find({$or: [{firstName: search}, {lastName: search}]})
+      .then(customer => {
+        console.info(customer);
+        console.info(`${customer.length} matches`);
+        mongoose.connection.close()
+      });
+  }
+
+//Update Customer
+const updateCustomer = async (_id, customer) => {
+  try {
+      Customer.updateOne({_id}, customer)
+      .then(customer => {
+        console.info('Customer Updated');
+        mongoose.connection.close()
+      });
+
+  } catch ( err ) {
+      console.info(err)
+  }
+ }
+
+
+
+//Remove Customer
+const removeCustomer = (_id) => {
+  Customer.remove({_id})
+  .then(customer => {
+    console.info('Customer Dihapus')
+    mongoose.connection.close()
+  })
+  .catch(err => {
+    console.info(err)
+  }) 
+}
+
+//List Customer 
+const listCustomer = () => {
+  Customer.find()
+    .then(customers => {
+      console.info(customers)
+      console.info(`${customers.length} customers`)
+      mongoose.connection.close()
+    })
+    .catch(err => {
+      console.info(err)
+    }) 
+}
+
+
+//export all module
+module.exports = {
+    addCustomer,
+    findCustomer,
+    updateCustomer,
+    removeCustomer,
+    listCustomer
+}
